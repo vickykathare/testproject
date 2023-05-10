6f53861468068ce4ecbc4ee058af6507bf7c0bea
SAIKUMAR <SAIKUMAR@192.168.86.159>
2023-05-10 12:10:03 +0530

    integaers

---
+++ integers/src/integers/integers.java
@@ -0,0 +1,15 @@
+package integers;
+
+public class integers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int c; //declaring a variable
+		 /* Using for loop to repeat instruction execution */
+		 for (c = 1; c <= 10; c++) {
+		 System.out.println(c);
+		 }
+
+	}
+
+}
